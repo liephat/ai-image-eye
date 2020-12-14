b788e165333c4b3f9cae36d3df801bf6926a9d73
Jörg <jeremyjove@gmail.com>
2020-12-14 22:05:39 +0100

    #7 pylint-exit saves the day

--- pylint.sh
+++ pylint.sh
@@ -2,14 +2,4 @@
 
 FILES=$(find . -type f -name "*.py")
 
-python -m pylint $FILES
-
-EXITCODE=$?
-
-if [ $EXITCODE -eq 4 -o $EXITCODE -eq 12 ]; then
-  # don't fail on Warnings, Refactorings
-  echo "Overriding exit code $EXITCODE from pylint with 0"
-  exit 0
-else
-  exit $EXITCODE
-fi
+python -m pylint $FILES || pylint-exit $?
