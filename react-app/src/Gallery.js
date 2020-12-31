a9879399f324b3cd01c1581da01e10bebfe32e6c
Jörg <jeremyjove@gmail.com>
2020-12-31 23:25:59 +0100

    #15 load more images on scrolling instead button

--- react-app/src/Gallery.js
+++ react-app/src/Gallery.js
@@ -1,37 +1,56 @@
 import React from 'react';
 import ImageThumbnail from './ImageThumbnail';
-import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 
 class Gallery extends React.Component {
-    state = {
-        images: [],
-        numLoaded: 3,
-    }
 
     constructor(props) {
         super(props);
-        this.loadMore = this.loadMore.bind(this);
+        this._loadMore = this._loadMore.bind(this);
+        this.state = {
+            images: [],
+            numLoaded: 0,
+        }
     }
 
     componentDidMount() {
         fetch('/all_images').then(res => res.json()).then(data => {
             console.log(data)
             this.setState({
-                images: data.images
+                images: data.images,
+                numLoaded: 6,
             });
         });
+        this._installScrollObserver();
     }
 
-    loadMore() {
-        let numLoadMore = this.state.numLoaded + 3
+    _installScrollObserver() {
+        var options = {
+            root: null,
+            rootMargin: "0px",
+            threshold: 1.0
+        };
+
+        this.observer = new IntersectionObserver(
+            this._loadMore,
+            options
+        );
+        this.observer.observe(this.loadingRef);
+    }
+
+    _loadMore() {
+        let numLoadMore = Math.min(this.state.numLoaded + 3, this.state.images.length);
         this.setState({
             numLoaded: numLoadMore,
         })
     }
 
     render() {
+        const loadingCss = {
+            height: "100px",
+            margin: "30px",
+        }
         return (
         <>
             <Row>
@@ -41,7 +60,9 @@ class Gallery extends React.Component {
             </Row>
             <Row className="justify-content-center">
                 <Col xs="auto">
-                    <Button onClick={this.loadMore}>Load more ({this.state.numLoaded}/{this.state.images.length})</Button>
+                    <div ref={loadingRef => (this.loadingRef = loadingRef)} style={loadingCss}>
+                        ({this.state.numLoaded}/{this.state.images.length})
+                    </div>
                 </Col>
             </Row>
         </>
