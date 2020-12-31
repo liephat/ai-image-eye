a4a3e05aa818034af4767a398aebe3dac6a8092d
Jörg <jeremyjove@gmail.com>
2020-12-31 22:51:26 +0100

    #15 cleanup code

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,5 +1,4 @@
-import logo from './logo.svg';
-//import './App.css';
+import './App.css';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Gallery from './Gallery';
