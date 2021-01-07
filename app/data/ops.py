faae69c32587c8d3248bf6b3c6bdff158da5e2fb
Mike Imhof <mike.imhof@uni-bamberg.de>
2021-01-07 21:03:05 +0100

    #2 add mechanism to close session after transactions

--- app/data/ops.py
+++ app/data/ops.py
@@ -1,4 +1,5 @@
 import os
+from contextlib import contextmanager
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -8,12 +9,29 @@ from app.data.ids import create_id
 from app.data.models import Image, Label, Base
 
 
+@contextmanager
+def auto_session(Session):
+    session = Session()
+    try:
+        yield session
+        session.commit()
+    except:
+        session.rollback()
+    finally:
+        session.close()
+
+
 class ImageDataHandler:
 
     def __init__(self):
-        Config = ConfigParser()
+        config = ConfigParser()
 
-        self.session = create_session(Config.data_file())
+        filepath = os.path.join(os.getcwd(), config.data_file())
+        engine = create_engine(f"sqlite:////{filepath}")
+        Base.metadata.create_all(bind=engine)
+
+        self.Session = sessionmaker()
+        self.Session.configure(bind=engine)
 
     def add_new_image(self, file, label_names):
         """
@@ -22,80 +40,52 @@ class ImageDataHandler:
         :param label_names: list of labels describing the image
         """
 
-        # Check if image exists
-        image = (
-            self.session.query(Image)
-                .filter(Image.file == file)
-                .one_or_none()
-        )
-
-        # Get the labels
-        labels = []
-        for label_name in label_names:
-            label = (
-                self.session.query(Label)
-                    .filter(Label.name == label_name)
+        with auto_session(self.Session) as session:
+            # Check if image exists
+            image = (
+                session.query(Image)
+                    .filter(Image.file == file)
                     .one_or_none()
             )
-            # Do we need to create the label?
-            if label is None:
-                label = Label(label_id=create_id(), name=label_name)
-                self.session.add(label)
 
-            labels.append(label)
+            # Get the labels
+            labels = []
+            for label_name in label_names:
+                label = (
+                    session.query(Label)
+                        .filter(Label.name == label_name)
+                        .one_or_none()
+                )
+                # Do we need to create the label?
+                if label is None:
+                    label = Label(label_id=create_id(), name=label_name)
+                    session.add(label)
 
-        # Create image if it not exists
-        if image is None:
-            image = Image(image_id=create_id(), file=file)
-            self.session.add(image)
+                labels.append(label)
 
-        image.labels += labels
+            # Create image if it not exists
+            if image is None:
+                image = Image(image_id=create_id(), file=file)
+                session.add(image)
 
-        # Commit to the database
-        self.session.commit()
-
-    def get_images(self):
-        """
-        Returns all image objects from database.
-        """
-        return self.session.query(Image).all()
-
-    def get_labels(self):
-        """
-        Returns all label objects from database.
-        """
-        return self.session.query(Label).all()
+            image.labels += labels
 
     def get_labellist_for_image(self, file):
         """
         Returns list of labels for one image.
         :param file: relative path to image file within the image folder
         """
-        image = (
-            self.session.query(Image)
-                .filter(Image.file == file)
-                .one_or_none()
-        )
-        return [label.name for label in image.labels]
+        with auto_session(self.Session) as session:
+            image = (
+                session.query(Image)
+                    .filter(Image.file == file)
+                    .one_or_none()
+            )
+            return [label.name for label in image.labels]
 
     def filelist(self):
         """
         Returns a list of all image files.
         """
-        return [file for (file,) in self.session.query(Image.file).all()]
-
-
-def create_session(file):
-    """
-    Creates and returns a database session.
-    :param file: relative path to sqlite file within working directory
-    """
-    filepath = os.path.join(os.getcwd(), file)
-
-    engine = create_engine(f"sqlite:////{filepath}")
-    Base.metadata.create_all(bind=engine)
-
-    Session = sessionmaker()
-    Session.configure(bind=engine)
-
-    return Session()
+        with auto_session(self.Session) as session:
+            return [file for (file,) in session.query(Image.file).all()]
