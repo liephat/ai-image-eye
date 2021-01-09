a6bdb64365104c6a98c60e465c4032526786af30
Jörg <jeremyjove@gmail.com>
2021-01-09 15:43:24 +0100

    #25 replace dependency: flask-restful -> flask-restx

--- app/__main__.py
+++ app/__main__.py
@@ -1,18 +1,16 @@
 import logging
 
-from flask import Flask, render_template, send_from_directory, jsonify
-from flask_restful import Api, Resource
+from flask import Flask, render_template, send_from_directory
 
+from app.config.parser import ConfigParser
 from app.data.ops import ImageDataHandler
 from app.ui.filters import init_filters, unescape_url
-from app.config.parser import ConfigParser
 
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s [%(levelname)s]: %(message)s')
 logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
-api = Api(app)
 
 # Load application configurations
 config = ConfigParser()
