4ac9c3930fa6a79df284449051c6ac0acebeca7e
Jörg <jeremyjove@gmail.com>
2021-01-17 22:32:45 +0100

    #4 Workaround: SQLAlchemy sessions are not thread safe

--- app/__main__.py
+++ app/__main__.py
@@ -7,4 +7,6 @@ logger = logging.getLogger(__name__)
 
 if __name__ == '__main__':
     app = AppWrapper().init_flask_app()
-    app.run(debug=True, port=5000)
+    # FIXME: threaded=False is a workaround for SQLAlchemy problems with sessions in multiple
+    # threads
+    app.run(debug=True, port=5000, threaded=False)
