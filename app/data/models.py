b1d008078be68e0e74576fa01394e7c2134115f4
Mike Imhof <mike.imhof@uni-bamberg.de>
2021-02-05 21:51:43 +0100

    #28 add label assignment class

--- app/data/models.py
+++ app/data/models.py
@@ -1,16 +1,9 @@
-from sqlalchemy import Column, String, ForeignKey, Table, Integer
+from sqlalchemy import Column, String, ForeignKey, Integer, Float, DateTime, func
 from sqlalchemy.ext.hybrid import hybrid_property
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
-
-image_label = Table(
-    "image_label",
-    Base.metadata,
-    Column("image_id", String, ForeignKey("image.image_id")),
-    Column("label_id", String, ForeignKey("label.label_id"))
-)
 
 
 class Image(Base):
@@ -18,7 +11,7 @@ class Image(Base):
     id = Column(Integer, primary_key=True)
     image_id = Column(String, nullable=False)
     file = Column(String, nullable=False, unique=True)
-    labels = relationship("Label", secondary=image_label, back_populates="images")
+    labels = relationship("Label", secondary="label_assignment")
 
     def __repr__(self):
         return f"Image('{self.id}', '{self.image_id}', '{self.file}', '{self.labels}')"
@@ -38,7 +31,25 @@ class Label(Base):
     id = Column(Integer, primary_key=True)
     label_id = Column(String, nullable=False)
     name = Column(String, nullable=False, unique=True)
-    images = relationship("Image", secondary=image_label, back_populates="labels")
+    images = relationship("Image", secondary="label_assignment")
 
     def __repr__(self):
         return f"Label('{self.id}', '{self.label_id}', '{self.name}')"
+
+
+class LabelAssignment(Base):
+    __tablename__ = "label_assignment"
+    id = Column(Integer, primary_key=True)
+    image_id = Column(String, ForeignKey("image.image_id"))
+    label_id = Column(String, ForeignKey("label.label_id"))
+    origin = Column(String(256), nullable=False)
+    creation_time = Column(DateTime, default=func.now())
+    confidence = Column(Float)
+    bounding_boxes = Column(String)
+
+    image = relationship("Image", backref=backref("image_assoc"))
+    label = relationship("Label", backref=backref("label_assoc"))
+
+    def __repr__(self):
+        return f"Label('{self.id}', '{self.image_id}', '{self.label_id}', '{self.origin}', '{self.creation_time}', " \
+               f"'{self.confidence}', '{self.bounding_boxes}')"
