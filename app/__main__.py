59e5d140098f938992dc33b616730ecf41a4fbbc
Jörg <jeremyjove@gmail.com>
2021-02-07 21:59:34 +0100

    #3 get rid of proxy in packages.json
    * instead allow Cross Origin Resource Sharing in dev environment

--- app/__main__.py
+++ app/__main__.py
@@ -6,7 +6,8 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s [%(levelname)s]: %(m
 logger = logging.getLogger(__name__)
 
 if __name__ == '__main__':
-    app = AppWrapper().init_flask_app()
+    debug = True
+    app = AppWrapper(debug).init_flask_app()
     # FIXME: threaded=False is a workaround for SQLAlchemy problems with sessions in multiple
     # threads
-    app.run(debug=True, port=5000, threaded=False)
+    app.run(debug=debug, port=5000, threaded=False)
