65ef87a5f1a692ddc8cbd4fbe5774fe9f65d55a4
Jörg <jeremyjove@gmail.com>
2021-02-14 15:26:10 +0100

    #34 enable debugging via environment variable

--- app/__main__.py
+++ app/__main__.py
@@ -1,6 +1,10 @@
+import os
+
 if __name__ == '__main__':
     from app.main import app
-    debug = True
+
+    os.environ['FLASK_DEBUG'] = '1'
+
     # FIXME: threaded=False is a workaround for SQLAlchemy problems with sessions in multiple
     # threads
-    app.run(debug=debug, host='0.0.0.0', port=5000, threaded=False)
+    app.run(host='0.0.0.0', port=5000, threaded=False)
